b400a9462207b1d1ddeb9b938584d169eacb9883
Dumars-Romulus <Dumarsrom@gmail.com>
2020-08-03 16:59:30 -0400

    Added top-content to main-content

--- js/index.js
+++ js/index.js
@@ -39,9 +39,6 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 document.getElementById("logo-img").setAttribute('src', siteContent["nav"]["img-src"]);
-document.getElementById("cta-img").setAttribute('src', siteContent["cta"]["img-src"]);
-document.getElementById("middle-img").setAttribute('src',siteContent["main-content"]["middle-img-src"]);
-
 document.querySelector("nav :nth-child(1)").textContent = siteContent["nav"]["nav-item-1"];
 document.querySelector("nav :nth-child(2)").textContent = siteContent["nav"]["nav-item-2"];
 document.querySelector("nav :nth-child(3)").textContent = siteContent["nav"]["nav-item-3"];
@@ -51,3 +48,10 @@ document.querySelector("nav :nth-child(6)").textContent = siteContent["nav"]["na
 
 document.querySelector(".cta-text h1").textContent = siteContent["cta"]["h1"];
 document.querySelector(".cta-text button").textContent = siteContent["cta"]["button"];
+document.getElementById("cta-img").setAttribute('src', siteContent["cta"]["img-src"]);
+
+document.getElementById("middle-img").setAttribute('src',siteContent["main-content"]["middle-img-src"]);
+document.querySelector(".top-content .text-content:nth-child(1) h4").textContent = siteContent["main-content"]["features-h4"];
+document.querySelector(".top-content .text-content:nth-child(1) p").textContent = siteContent["main-content"]["features-content"];
+document.querySelector(".top-content .text-content:nth-child(2) h4").textContent = siteContent["main-content"]["about-h4"];
+document.querySelector(".top-content .text-content:nth-child(2) p").textContent = siteContent["main-content"]["about-content"];
